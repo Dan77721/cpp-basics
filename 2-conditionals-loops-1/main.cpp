fa2508f9221f5c5c720972b2333e3f6e6ccb639c
DAN <d.balioz@mail.ru>
2018-10-24 19:54:40 +0300

    labs 2 new

--- 2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/main.cpp
@@ -1,52 +1,78 @@
 #include <iostream>
-#include <cmath>
 #include <iomanip>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-	double Xf, Xl, dX, a, b, c, f, X;
-	int expression;
+int main() {
+	const double kEps = 1e-15;
+
+	double a, b, c, xn, xk, dx;
 	cout << "Enter a: ";
 	cin >> a;
 	cout << "Enter b: ";
 	cin >> b;
 	cout << "Enter c: ";
 	cin >> c;
-	cout << "Enter X first: ";
-	cin >> Xf;
-	cout << "Enter X last: ";
-	cin >> Xl;
-	cout << "Enter dX: ";
-	cin >> dX;
-	cout << "-------------------------------------" << endl;
-	cout << "|        X        |        F        |" << endl;
-	cout << "-------------------------------------" << endl;
-	if ((int(a) | int(b) & (int(a) | int(c)))) expression = 1;
-	else expression = 0;
-	cout.precision(3);
-	cout << fixed;
-	for (X = Xf; X <= Xl; X += dX) {
-		cout  << "|" << setw(12) << X ;
-		if (X < 0 && b != 0) f = a * X * X + b;
-		else if (X > 0 && b == 0) {
-			if (X - c != 0) f = (X - a) / (X - c);
-			else {
-				cout << "ERROR" << endl;
-				break;
-			}
-		}
-		else if (c != 0) f = X / c;
-		else {
-			cout << "ERROR" << endl;
-			break;
-		}
-		cout << "     |" ;
-		cout.width(12);
-		if (expression == 1) cout << f ;
-		else cout << int(f) ;
+	cout << "Enter xn: ";
+	cin >> xn;
+	cout << "Enter xk >= xn: ";
+	cin >> xk;
+	cout << "Enter dx > 0: ";
+	cin >> dx;
+
+	if (dx <= 0) {
+		cout << "\nInvalid dx. Must be: dx > 0.\n";
 	}
-	cout << "     |" << endl;
-	cout << "-------------------------------------";
+	else if (xn > xk) {
+		cout << "\nInvalid xk. Must be: xk >= xn.\n";
+	}
+	else
+	{
+		cout << string(37, '-') << endl;
+		cout << "|        x        |        F        |\n";
+		cout << string(37, '-') << endl;
+
+		cout << fixed;
+		cout.precision(3);
+
+		for (double x = xn; x <= xk; x += dx) {
+			cout << "|" << setw(12) << x << setw(6) << "|";
+
+			double f;
+			if (x < 0 && abs(b) >= kEps) {
+				f = a * x * x + b;
+			}
+			else if (x > 0 && abs(b) < kEps) {
+				if (abs(x - c) >= kEps) {
+					f = (x - a) / (x - c);
+				}
+				else {
+					cout << "  division by 0  |\n";
+					continue;
+				}
+			}
+			else {
+				if (abs(c) >= kEps) {
+					f = x / c;
+				}
+				else {
+					cout << "  division by 0  |\n";
+					continue;
+				}
+			}
+
+			int ac = static_cast<int>(a);
+			int bc = static_cast<int>(b);
+			int cc = static_cast<int>(c);
+			if ((ac | bc) & (ac | cc))
+				cout << setw(12) << f << setw(7);
+			else
+				cout << setw(10) << static_cast<int>(f) << setw(9);
+			cout << "|\n";
+		}
+		cout << string(37, '-');
+	}
+
 	return 0;
 }
